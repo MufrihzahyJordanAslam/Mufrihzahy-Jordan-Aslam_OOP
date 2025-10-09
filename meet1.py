0892988ff3e85d91b482a5b64d766e6239d14dae
Jordan <mufrihjordan@gmail.com>
2025-10-10 01:35:32 +0800

    Add files via upload

---
+++ meet1.py
@@ -0,0 +1,14 @@
+# Menyimpan data mahasiswa dalam variabel
+nim = "242421167"
+nama = "Mufrihzahy Jordan Aslam"
+jurusan = "Pendidikan Teknologi Informasi"
+angkatan = 2024
+ipk = 3.86
+
+# Menampilkan data mahasiswa
+print("Data Mahasiswa:")
+print("NIM       :", nim)
+print("Nama      :", nama)
+print("Jurusan   :", jurusan)
+print("Angkatan  :", angkatan)
+print("IPK       :", ipk)
